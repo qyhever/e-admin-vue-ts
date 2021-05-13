8b3b70986152a2b3043cbfb935e3902aa9eb9039
v_yohqin <v_yohqin@tencent.com>
2021-05-13 10:28:22 +0800

    excel

---
+++ src/views/excel/select-export-excel.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="com-page p20">选择导出</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  setup() {
+    console.log('setup')
+  }
+})
+</script>
+
+<style lang="less" scoped>
+// ...
+</style>
