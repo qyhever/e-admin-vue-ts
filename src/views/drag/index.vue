9a34f45b0e133609b01945cab670a7b1d7f8ad01
v_yohqin <v_yohqin@tencent.com>
2021-06-04 22:16:48 +0800

    add emoji

--- src/views/drag/index.vue
+++ src/views/drag/index.vue
@@ -3,6 +3,12 @@
 </template>
 
 <script lang="ts">
+// https://jbaysolutions.github.io/vue-grid-layout/guide/10-drag-from-outside.html
+// https://github.com/jbaysolutions/vue-grid-layout/tree/vue3/src
+// https://github.com/lvjunhao/vue-grid-layout/tree/main/src
+// https://www.npmjs.com/package/vue-draggable-next
+
+// https://github.com/dattn/dnd-grid
 import { defineComponent } from 'vue'
 export default defineComponent({
   setup() {
